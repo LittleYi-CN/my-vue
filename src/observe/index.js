faaafc1ac451fbc5251cdcfc8e908ae8c0c949a6
仪泉江 <sdgmyqj@163.com>
2022-07-01 17:15:18 +0800

    ✨ 4、实现数组的函数劫持

--- src/observe/index.js
+++ src/observe/index.js
@@ -1,12 +1,32 @@
+import { newArrayProto } from "./array";
+
 class Observer {
   constructor(data) {
     // Object.defineProperty 只能劫持已经存在的属性（vue里面会为此单独写一些api $set $delete）
-    this.walk(data);
+
+    Object.defineProperty(data, '__ob__', { // 给数据加了一个标识， 如果数据上有__ob__说明这个属性被观测
+      value: this,
+      enumerable: false // 将__ob__变成不可枚举的
+    });
+
+    if(Array.isArray(data)) {
+      // 这里可以重写数组中的方法 7个变异方法 这些方法是可以修改数组本身的
+      // push() pop() shift() unshift() splice() reverse() sort()
+      data.__proto__ = newArrayProto // 需要保留数组原有的特性，并且可以重写部分方法
+      this.observeArray(data); // 如果数组中放的是对象，可以监控到对象的变化
+    }else {
+      this.walk(data);
+    }
   }
 
   walk(data) { // 循环对象，对属性依次劫持
     // 重新定义属性
     Object.keys(data).forEach(key => defineReactive(data, key, data[key]));
+  }
+
+  // 
+  observeArray(data) {
+    data.forEach(item => observe(item))
   }
 }
 
@@ -18,6 +38,7 @@ export function defineReactive(target, key, value) { // 闭包 属性劫持
     },
     set(newValue) { // 修改的时候会执行set
       if(newValue === value) return;
+      observe(newValue);
       value = newValue
     }
   })
@@ -29,6 +50,9 @@ export function observe(data) {
     return; // 只对对象进行劫持
   }
 
+  if(data.__ob__ instanceof Observer) { // 说明这个对象已经被代理过了
+    return data.__ob__;
+  }
   // 如果一个对象被劫持过了，那就不需要再被劫持了（要判断 一个对象是否被劫持过，可以增添一个实例，用实例来判断是否被劫持过）
 
   return new Observer(data);
